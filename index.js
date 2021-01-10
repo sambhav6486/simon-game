f4a39702ff857c543552e876a775486f76df8a04
sambhav6496 <jainsam620@gmail.com>
2021-01-10 21:06:57 +0530

    simon game

--- index.js
+++ index.js
@@ -1,68 +1,72 @@
-var body = $('body')
-var btn =$(".btn")
-var heading = $('h1')
+var body = $("body");
+var btn = $(".btn");
+var heading = $("h1");
 var started = false;
-pattern = []
+var pattern = [];
 var level = 0;
-var count = 0
+var count = 0;
 
-body.keypress(function(){
-    if(!started){
-        generateRandomNumber()
-        buttonClick()
+body.keypress(function () {
+  if(!started) {
+    generateRandomNumber();
+    buttonClick();
+  }
+  started = true;
+});
+
+function buttonClick() {
+  btn.click(function () {
+    var btnId = this.id;
+    animateButton(btnId);
+    checkAnswer(btnId);
+  });
+}
+
+function checkAnswer(userInput) {
+  console.log(pattern[count], userInput);
+  if(count < pattern.length){
+    if(userInput != pattern[count]){
+      changeHeading("the game is over");
+      setTimeout(function(){
+        restart()
+      },300)
+    }else{
+        count++
     }
-    started = true
-})
+  }
 
-function buttonClick(){
-    btn.click(function(){
-        var btnId = this.id
-       animateButton(btnId)
-       checkAnswer(btnId)
-    })
-}
-
-function checkAnswer(userInput){
-    console.log(pattern[count] , userInput)
-   if(count < pattern.lenght){
-       if(userInput != pattern[count]){
-               changeHeading("the game is over")
-               setTimeout(function(){
-                restart()
-               },300)   
-       }
-   }
-   count++
-
-   if(count == pattern.length){
-       count = 0 
-       generateRandomNumber()
-   }
-}
-
-
-function generateRandomNumber(){
- level++
-  var randomNumber = Math.floor(Math.random() * 4)
-  pattern.push(randomNumber)
-  animateButton(randomNumber)
-  changeHeading("level " + level)
-}
-
-function animateButton(value){
-    btn.eq(value).addClass('random')
+  if (count == pattern.length) {
+    count = 0;
     setTimeout(function(){
-        btn.eq(value).removeClass('random')
+        generateRandomNumber();    
     },300)
+    
+  }
 }
 
-function changeHeading(data){
-   heading.text(data)
+function generateRandomNumber() {
+  level++;
+  var randomNumber = Math.floor(Math.random() * 4);
+  pattern.push(randomNumber);
+  animateButton(randomNumber);
+  changeHeading("level " + level);
 }
 
-function restart(){
-    changeHeading("Press a key to start game")
-  pattern = []
-  started = false;
+function animateButton(value) {
+  btn.eq(value).addClass("random");
+  setTimeout(function () {
+    btn.eq(value).removeClass("random");
+  }, 300);
+}
+
+function changeHeading(data) {
+  heading.text(data);
+}
+
+function restart() {
+  changeHeading("Press a key to start game");
+  pattern = [];
+  level = 0
   count = 0
+  started = false;
 }
